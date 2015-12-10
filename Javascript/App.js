3d5b33a35d4fb42d92568695c9403c2712682c62
ArjaaAine <arjaaaine@gmail.com>
2015-12-09 22:36:17 -0800

    Base Setup
    Setup Architecture and Intro Screens

---
+++ Javascript/App.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+
+var wtpApp = angular.module(
+    'WWTP',
+    [
+        'ui.bootstrap',
+        'ngRoute',
+        'ngSanitize',
+        'ui.select'
+    ]
+);
+
+wtpApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/changelog',
+    {
+        templateUrl: 'Views/changelog.html',
+        controller: 'ChangelogController',
+        controllerAs: 'cl'
+    })
+    .when('/ia',
+    {
+        templateUrl: 'Views/internalAffairs.html',
+        controller: 'CountryController',
+        controllerAs: 'countryCtrl'
+    })
+    .when('/structure',
+    {
+        templateUrl: 'Views/structures.html',
+        controller: 'StructureController',
+        controllerAs: 'structureCtrl'
+    })
+    .when('/military',
+    {
+        templateUrl: 'Views/military.html',
+        controller: 'MilitaryController',
+        controllerAs: 'militaryCtrl'
+    })
+    .when('/research',
+    {
+        templateUrl: 'Views/research.html',
+        controller: 'ResearchController',
+        controllerAs: 're'
+    })
+    .when('/war',
+    {
+        templateUrl: 'Views/war.html',
+        controller: 'WarController',
+        controllerAs: 'war'
+    })
+    .otherwise({
+        redirectTo: '/ia'
+    });
+
+});
---
+++ Javascript/Controllers/GameController.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+
+wtpApp.controller(
+    'GameController',
+    function (
+        $scope,
+        $interval
+       ) {
+
+        var game = this;
+        game.data = {
+            init: {
+                isFirstTime: true
+            }
+        };
+
+        game.validation = {
+            initLeaderName: true,
+            initPartySelection: true
+        }
+
+        game.baseStats = {
+            selectedParty: {},
+            leaderName: ''
+        }
+
+        game.lookup = {
+        };
+
+        game.lookup.parties = [
+            {
+                name: 'Republican Party',
+                selectionText: 'Congrats! You selected the party worthy of a patriot!'
+            },
+            {
+                name: 'Democratic Party',
+                selectionText: 'Are you a freeloading socialist? Select the true party for the patriots!'
+            }
+        ]
+
+    });
