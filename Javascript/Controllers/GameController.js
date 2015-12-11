ee2e869d96360717ff3441aae148c0afac888e78
ArjaaAine <arjaaaine@gmail.com>
2015-12-11 00:23:03 -0800

    Added Situations
    Added SituationData Service and built UI and Data Structure to show the
    situations.

--- Javascript/Controllers/GameController.js
+++ Javascript/Controllers/GameController.js
@@ -4,28 +4,55 @@ wtpApp.controller(
     'GameController',
     function (
         $scope,
-        $interval
+        $interval,
+        situationData
        ) {
 
         var game = this;
-        game.data = {
-            init: {
-                isFirstTime: true
-            }
+
+        game.situation = situationData;
+
+        var saveGame = function () {
+            game.situation.functions.saveData();
+            localStorage['gameData'] = JSON.stringify(game.data);
         };
+        var resetGame = function () {
+            game.data = {
+                init: {
+                    isFirstTime: true
+                }
+            };
+            game.situation.functions.resetData();
+            localStorage.clear();
+        };
+        var saveTimer = $interval(saveGame, 5000);
+
+        //Load Game's Settings
+        if (!localStorage['gameData']) {
+            game.data = {
+                init: {
+                    isFirstTime: false
+                }
+            };
+        }
+        else {
+            game.data = JSON.parse(localStorage['gameData']);
+        }
 
         game.validation = {
             initLeaderName: true,
             initPartySelection: true
         }
-
         game.baseStats = {
-            leaderName: ''
+            leaderName: 'Test',
+            selectedParty: {
+                name: 'Republican Party',
+                selectionText: 'Congrats! You selected the party worthy of a patriot!'
+            }
         }
 
         game.lookup = {
         };
-
         game.lookup.parties = [
             {
                 name: 'Republican Party',
@@ -37,8 +64,26 @@ wtpApp.controller(
             }
         ]
 
+        game.selectedChoice = function (situationId, choiceId) {
+
+            var situation = $.grep(game.situation.baseData.situations, function (sit) {
+                return sit.SituationId == situationId;
+            });
+            game.data.activeSituation = situation[0];
+
+            if (game.data.activeSituation.choices) {
+                for (var i = 0; i < game.data.activeSituation.choices.length; i++) {
+                    var result = $.grep(game.situation.baseData.choices, function (choice) {
+                        return choice.ChoiceId == game.data.activeSituation.choices[i].ChoiceId;
+                    });
+
+                    game.data.activeSituation.choices[i].ChoiceText = result[0].ChoiceText;
+                }
+            }
+        };
+
         game.startGame = function () {
-            if (game.baseStats.leaderName.length === 0 ) {
+            if (game.baseStats.leaderName.length === 0) {
                 game.validation.initLeaderName = false;
             } else {
                 game.validation.initLeaderName = true;
@@ -52,6 +97,27 @@ wtpApp.controller(
             } else {
                 game.validation.initPartySelection = true;
             }
+
+            if (game.validation.initLeaderName && game.validation.initPartySelection) {
+                game.data.init.isFirstTime = false;
+            }
+
+            //Setting up the first situation
+            game.data.activeSituation = game.situation.baseData.situations[0];
+
+            for (var i = 0; i < game.data.activeSituation.choices.length; i++) {
+                var result = $.grep(game.situation.baseData.choices, function (choice) {
+                    return choice.ChoiceId == game.data.activeSituation.choices[i].ChoiceId;
+                });
+
+                game.data.activeSituation.choices[i].ChoiceText = result[0].ChoiceText;
+            }
+        };
+        game.saveGame = function () {
+            saveGame();
+        };
+        game.resetGame = function () {
+            resetGame();
         };
 
     });
---
+++ Javascript/Services/SituationData.js
@@ -0,0 +1,85 @@
+﻿'use strict';
+
+wtpApp.factory('situationData', function () {
+
+    var situations = {
+        baseData: {},
+        functions: {}
+    }
+
+    //First Load
+    if (!localStorage['situationData']) {
+        setInitialSituationData(situations);
+    }
+    else {
+        situations.baseData = JSON.parse(localStorage['situationData']);
+    }
+
+    situations.functions.saveData = function () {
+        localStorage['situationData'] = JSON.stringify(situations.baseData);
+    };
+    situations.functions.resetData = function () {
+        setInitialSituationData(situations);
+    };
+
+    return situations;
+
+});
+
+var setInitialSituationData = function (situations) {
+    situations.baseData = {
+        situations: [
+            {
+                SituationId: 1,
+                SituationHeader: 'Initiation',
+                SituationText: 'Are you ready to be a GOP candidate?',
+                choices: [
+                    {
+                        ChoiceId: 1,
+                        ResultSituationId: 2
+                    },
+                    {
+                        ChoiceId: 2,
+                        ResultSituationId: 2
+                    },
+                    {
+                        ChoiceId: 3,
+                        ResultSituationId: 2
+                    },
+                    {
+                        ChoiceId: 4,
+                        ResultSituationId: 2
+                    }
+                ]
+            },
+            {
+                SituationId: 2,
+                SituationHeader: 'Game Over',
+                SituationText: 'Why did you play the game then?'
+            }
+        ],
+        choices: [
+            {
+                ChoiceId: 1,
+                ChoiceText: 'Yes'
+            },
+            {
+                ChoiceId: 2,
+                ChoiceText: 'No'
+            },
+            {
+                ChoiceId: 3,
+                ChoiceText: 'Maybe?'
+            },
+            {
+                ChoiceId: 4,
+                ChoiceText: 'Leave me alone!'
+            },
+            {
+                ChoiceId: 5,
+                ChoiceText: 'Game Over!'
+            }
+        ]
+    }
+
+};
